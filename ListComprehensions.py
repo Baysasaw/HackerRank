396f35db520d0bfdd2c1564358f36be28d7d57a9
yonas <104759558+Baysasaw@users.noreply.github.com>
2024-05-15 13:11:00 +0300

    List Comprehensions

---
+++ ListComprehensions.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+
+    Permutations = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1)]
+
+    ans = [Permutation for Permutation in Permutations if sum(Permutation) != n]
+
+
+    print(Permutations)
